855c307af522dec0bbd1fbab082770f9bb895507
chogerlate <siwaratlaopromger4444@gmail.com>
2024-02-21 00:04:20 +0700

    add pre-react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://main--chogerlate.netlify.app/"
+  site: "https://main--chogerlate.netlify.app/",
+  integrations: [preact()]
 });
